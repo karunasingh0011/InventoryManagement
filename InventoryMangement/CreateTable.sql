e4b5e5d52cc7e1abe6e364a6473cbb822a7444a0
karunasingh0011 <68496278+karunasingh0011@users.noreply.github.com>
2020-07-20 13:58:12 +0530

    Add files via upload

---
+++ InventoryMangement/CreateTable.sql
@@ -0,0 +1,13 @@
+
+
+
+CREATE TABLE [dbo].[InvInfo](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](200) NULL,
+	[Description] [varchar](500) NULL,
+	[Price] [decimal](10, 2) NULL,
+	[Photo] [varchar](max) NULL
+) 
+
+
+
